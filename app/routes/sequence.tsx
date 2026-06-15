cd91e1d99f55d5efd8cbd39fbe7532bf754c76e3
SadoudiAnais <sadoudianais11@gmail.com>
2026-06-15 14:17:45 +0200

    Update Sequence

--- app/routes/sequence.tsx
+++ app/routes/sequence.tsx
@@ -116,7 +116,9 @@ export default function Sequence() {
   }, []);
 
   const navItems = [
+    { label: "Home", href: "/" },
     { label: "Browse", href: "/browse" },
+    { label: "About", href: "/about" },
   ];
 
   return (
